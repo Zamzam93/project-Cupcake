088a6fb49dfd4d1c670c387e1f3076b96ddaeb2b
Zamzam93 <samira0101@live.com>
2022-09-28 14:05:00 -0500

    initial commit

---
+++ src/Chocalate.java
@@ -0,0 +1,6 @@
+public class Chocalate extends Cupcake {
+
+    public void type() {
+        System.out.println("A chocolate based cupcake, with chocolate frosting.");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList cupcakeMenu = new ArrayList<>();
+
+        //creating an object or creating instance
+        Cupcake cupcake = new Cupcake();
+
+        RedVelvet redVelvet = new RedVelvet();
+        Chocalate chocalate = new Chocalate();
+
+        System.out.println();
+
+        cupcake.type();
+        System.out.println("How much would you like to charge for the cupcake?");
+
+
+
+
+
+    }
+
+}
