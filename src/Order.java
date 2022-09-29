aa84a4b358ac73470ca6073711931286b66124ef
Zamzam93 <samira0101@live.com>
2022-09-29 17:10:59 -0500

    completed part3

--- src/Order.java
+++ src/Order.java
@@ -6,8 +6,8 @@ public class Order {
     public Order (ArrayList<Cupcake> cupcakeMenu, ArrayList<Drink> drinkMenu) {
         System.out.println("Hello customer. Would you like to place an order? (Y or N)");
 
-        Scanner scan = new Scanner(System.in);
-        String placeOrder = scan.nextLine();
+        Scanner input = new Scanner(System.in);
+        String placeOrder = input.nextLine();
 
         ArrayList order = new ArrayList<>();
 
@@ -59,6 +59,48 @@ public class Order {
         boolean ordering = true;
 
         while (ordering) {
+            System.out.println("What would you like to order? Please use the number associated with each item to order");
+
+            int orderChoice = input.nextInt();
+
+            input.nextLine();
+
+            if(orderChoice == 1){
+                order.add(cupcakeMenu.get(0));
+
+                System.out.println("Item added to order");
+            }else if (orderChoice == 2) {
+                order.add(cupcakeMenu.get(1));
+
+                System.out.println("Item added to order");
+            }else if (orderChoice == 3) {
+                order.add(cupcakeMenu.get(2));
+
+                System.out.println("Item added to order");
+            }else if(orderChoice == 4){
+                order.add(drinkMenu.get(0));
+
+                System.out.println("Item added to order");
+            }else if(orderChoice == 5){
+                order.add(drinkMenu.get(1));
+
+                System.out.println("Item added to order");
+            }else if(orderChoice == 6){
+                order.add(drinkMenu.get(2));
+
+                System.out.println("Item added to order");
+
+            }else {
+                System.out.println("Sorry, we don't seem to have that on the menu");
+            }
+
+            System.out.println("Would you like to continue ordering? (Y/N)");
+
+            String continueOrder = input.nextLine();
+
+            if (!continueOrder.equalsIgnoreCase("Y")){
+                ordering = false;
+            }
 
         }
 
